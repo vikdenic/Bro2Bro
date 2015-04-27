3e0da5f71dc91efc80322b55e3506e3df1649931
vik denic <vik.denic@gmail.com>
2015-04-26 19:55:28 -0500

    Initial Commit

---
+++ Bro2Bro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bro2Bro
+//
+//  Created by Vik Denic on 4/26/15.
+//  Copyright (c) 2015 nektar labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bro2BroTests/Bro2BroTests.m
@@ -0,0 +1,40 @@
+//
+//  Bro2BroTests.m
+//  Bro2BroTests
+//
+//  Created by Vik Denic on 4/26/15.
+//  Copyright (c) 2015 nektar labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Bro2BroTests : XCTestCase
+
+@end
+
+@implementation Bro2BroTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
